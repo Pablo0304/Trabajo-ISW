699cb1486796486529550ff672c15b4f03b27a6f
Pablo <gonca.pablo@gmail.com>
2022-10-11 18:31:14 +0200

    Paper y Magazine

--- Mereketengue/ClassLibrary/BusinessLogic/Entities/Magazine.cs
+++ Mereketengue/ClassLibrary/BusinessLogic/Entities/Magazine.cs
@@ -8,5 +8,19 @@ namespace ClassLibrary.Entities
 {
     public partial class Magazine
     {
+        public Magazine() { 
+            Issues = new List<Issue>();
+            Areas = new List<Area>();
+        }
+        public Magazine(String name, User usuario) { 
+            this.Name = Name;
+
+            // Relaciones a 1
+            User = Usuario;
+
+            // Colecciones
+            this.Issues = new List<Issue>();
+            this.Areas = new List<Area>();
+        }
     }
 }
--- Mereketengue/ClassLibrary/BusinessLogic/Entities/Paper.cs
+++ Mereketengue/ClassLibrary/BusinessLogic/Entities/Paper.cs
@@ -8,5 +8,20 @@ namespace ClassLibrary.Entities
 {
     public partial class Paper
     {
+        public Paper() { 
+        }
+        public Paper(String title, DateTime uploadDate, Area area, User user, Person coAuthors, Issue issue, Evaluation evaluacion) { 
+            this.Title = title;
+
+            // Relaciones a 1
+            User = usuario;
+            UploadDate = uploadDate;
+            Area = area;
+            CoAuthors = coAuthors;
+            Issue = issue;
+            evaluacion = evaluacion;
+
+        }
+    
     }
 }
