ee17ef383b66e9c173978eeba77508a7c43caba4
Javi <Jdieben@upv.edu.es>
2022-10-26 23:50:15 +0200

    Creados algunos objetos, no compila main de program

--- Mereketengue/DBTest2/Program.cs
+++ Mereketengue/DBTest2/Program.cs
@@ -1,23 +1,20 @@
-﻿using Magazine.Entities;
-using Magazine.Persistence;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Magazine.Entities;
+using Magazine.Persistence;
 
-namespace DBTest
+
+namespace DBTest2
 {
     internal class Program
     {
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
-            IDAL dal = new EntityFrameworkDAL(new MagazineDbContext());
 
-            createSampleDB(dal);
         }
-
         private void createSampleDB(IDAL dal)
         {
             // Remove all data from DB
@@ -26,32 +23,22 @@ namespace DBTest
             Console.WriteLine("Creant les dades i emmagatzemant en la BD");
             Console.WriteLine("===========================================");
             Console.WriteLine("\n// CREACIÓ D’UNA REVISTA i EL SEU EDITOR EN JEFE");
-            User u1 = new User("1234", "Pepe", "TheBoss", false, "ninguna", "pgarcia@gmail.com", "theboss", "1234");
+            User u1 = new User("1234", "Pepe", "TheBoss", false, "ninguna","pgarcia@gmail.com", "theboss", "1234");
             dal.Insert<User>(u1);
             dal.Commit();
             Magazine.Entities.Magazine m = new Magazine.Entities.Magazine("Revista Universitaria", u1);
-
+           
             u1.Magazine = m;
 
             dal.Insert<Magazine.Entities.Magazine>(m);
             dal.Commit();
             Console.WriteLine("Nom de la revista: " + m.Name);
-            Console.WriteLine(" Editor de la revista: " + m.ChiefEditor.Name + " " + m.ChiefEditor.Surname);
+            Console.WriteLine(" Editor de la revista: " + m.ChiefEditor.Name + " " +
+           m.ChiefEditor.Surname);
 
             Console.ReadKey();
             // Populate here the rest of the database with data
-            User u2 = new User("2345", "Aitor", "Tilla", false, "ninguna", "AitorTilla@gmail.com", "HayTortillla", "contrasena");
-            dal.Insert(u2);
-            dal.Commit();
-            Console.WriteLine("Ya hay tortilla");
-
-            User u3 = new User("4321", "Cindy", "Nero", false, "ninguna", "Sindinero@gmail.com", "MoreMoneyMoreProblems", "añesartnoc");
-            dal.Insert(u2);
-            dal.Commit();
-            Console.WriteLine("Cindy creada");
-
         }
-        
 
     }
 }
