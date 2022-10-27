bec33ea54682902a1e9ba1ecb6f9d0f9585f98fe
Javier Diéguez Bens <JDIEBEN@upv.edu.es>
2022-10-27 15:15:21 +0200

    Creación de objetos (menos issue)

--- Mereketengue/DBTest/Program.cs
+++ Mereketengue/DBTest/Program.cs
@@ -25,7 +25,8 @@ namespace DBTest
             dal.Insert<User>(u1);
             dal.Commit();
 
-            User u2 = new User("2345", "Aitor", "Tilla", false, "ninguna", "AitorTilla@gmail.com", "HayTortillla", "contrasena");
+
+            User u2 = new User("user", "Aitor", "Tilla", false, "ninguna", "AitorTilla@gmail.com", "HayTortillla", "contrasena");
             dal.Insert<User>(u2);
             Console.WriteLine("Ya hay tortilla");
             dal.Commit();
@@ -35,13 +36,36 @@ namespace DBTest
             dal.Commit();
             Console.WriteLine("Cindy creada");
             
-            Magazine.Entities.Magazine m = new Magazine.Entities.Magazine("Revista Universitaria", u1);
-            u1.Magazine = m;
-            dal.Insert<Magazine.Entities.Magazine>(m);
+            Magazine.Entities.Magazine m1 = new Magazine.Entities.Magazine("Revista Universitaria", u1);
+            u1.Magazine = m1;
+            dal.Insert<Magazine.Entities.Magazine>(m1);
             dal.Commit();
 
-            Console.WriteLine("Nom de la revista: " + m.Name);
-            Console.WriteLine(" Editor de la revista: " + m.ChiefEditor.Name + " " + m.ChiefEditor.Surname);
+            Area a1 = new Area("Deportes", u1, m1);
+            m1.Areas.Add(a1);
+            u1.Area = a1;   
+            dal.Insert<Area>(a1);
+            dal.Commit();
+
+            Paper p1 = new Paper("Jua Carlos", Convert.ToDateTime("01/03/2004"), a1, u2);
+            a1.Papers.Add(p1);
+            u3.MainAuthoredPapers.Add(p1);
+            u2.CoAuthoredPapers.Add(p1);
+            dal.Insert<Paper>(p1);
+            dal.Commit();
+
+            Evaluation e1 = new Evaluation(true, "ninguno", Convert.ToDateTime("01/06/2011"));
+            p1.Evaluation = e1;
+            dal.Insert<Evaluation>(e1);
+            dal.Commit();
+
+            Issue i1 = new Issue(12, m1);
+            p1.Issue = i1;
+            i1.PublishedPapers.Add(p1);
+            dal.Insert<Paper>(p1);
+            dal.Commit();
+
+
 
             Console.ReadKey();
             // Populate here the rest of the database with data
