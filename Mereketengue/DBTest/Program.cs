fc7c7419f584872ea9af794aca478dee6a674d5b
Javier Diéguez Bens <JDIEBEN@upv.edu.es>
2022-11-20 19:30:40 +0100

    Hagan todos pull, gracias

--- Mereketengue/DBTest/Program.cs
+++ Mereketengue/DBTest/Program.cs
@@ -66,9 +66,9 @@ namespace DBTest
             dal.Commit();
 
             Area a2 = new Area("Coches", u2, m1);
-            m1.Areas.Add(a1);
-            u2.Area = a1;
-            dal.Insert<Area>(a1);
+            m1.Areas.Add(a2);
+            u2.Area = a2;
+            dal.Insert<Area>(a2);
             dal.Commit();
 
 
