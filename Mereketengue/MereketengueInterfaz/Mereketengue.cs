32671af3652568d49950b37182a93d7bb914c7ba
Pablo <gonca.pablo@gmail.com>
2022-12-20 20:36:04 +0100

    Integración satisfactoria de IGU con código

---
+++ Mereketengue/MereketengueInterfaz/Mereketengue.cs
@@ -0,0 +1,23 @@
+﻿using Magazine.Services;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MereketengueInterfaz
+{
+    public partial class Mereketengue : Form
+    {
+        private IMagazineISWService service;
+        public Mereketengue(IMagazineISWService service)
+        {
+            InitializeComponent();
+            this.service = service;
+        }
+    }
+}
---
+++ Mereketengue/MereketengueInterfaz/Program.cs
@@ -0,0 +1,25 @@
+﻿using Magazine.Persistence;
+using Magazine.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MereketengueInterfaz
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Punto de entrada principal para la aplicación.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            IMagazineISWService service = new MagazineISWService(new EntityFrameworkDAL(new MagazineDbContext()));
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Mereketengue(service));
+        }
+    }
+}
