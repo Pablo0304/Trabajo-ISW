b8753dbeab245bcedfed9be213390f443bdb61af
Daniel Sánchez Tardy <DSANTAR@upv.edu.es>
2023-01-28 00:24:01 +0100

    ConsultarIssue codigo y ventana hechos

--- Mereketengue/MereketengueInterfaz/ConsultarIssueNOReg.cs
+++ Mereketengue/MereketengueInterfaz/ConsultarIssueNOReg.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Magazine.Entities;
+using Magazine.Services;
+using MereketengueInterfaz.Properties;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,14 +10,77 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace MereketengueInterfaz
 {
     public partial class ConsultarIssueNOReg : Form
     {
+        private IMagazineISWService service;
+        private Magazine.Entities.Magazine magazine;
+        private Area area;
+        private int valor;
+        private User usercontrol;
         public ConsultarIssueNOReg()
         {
             InitializeComponent();
+            this.buscador.Image = Resources.lupa;
+            this.service = service;
+            this.IssueNumberError.Text = "";
+            this.meterPrice.Text = "";
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void IssueNumberError_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cluckBuscador(object sender, EventArgs e)
+        {
+            try
+            {
+                IssueNumberError.Text = "";
+                valor = int.Parse(buscadorBox.Text);
+            }
+            catch (FormatException)
+            {
+                IssueNumberError.Text = "Thats not a valid number.";
+                buscador.Text = "";
+            }
+            catch (OverflowException)
+            {
+                IssueNumberError.Text = "That number is too big or too small, idk.";
+                buscador.Text = "";
+            }
+            ListaPapers.Items.Clear();
+            IssueNumberError.Text = "";
+            if (buscadorBox.Text.Length == 0)
+            {
+               foreach (Issue i in service.getAllIssues())
+               {
+                    if (i.Number == valor)
+                    {
+                        if (i.Number >= 0)
+                        {
+                            meterPrice.Text = i.Price + "€";
+                            foreach (Paper p in i.PublishedPapers)
+                            {
+                                String autores = "";
+                                foreach (Person per in p.CoAuthors)
+                                {
+                                    autores += per.Name.ToString();
+                                }
+                                ListaPapers.Items.Add("Title: " + p.Title + " | Authors: " + autores);
+                            }
+                        }
+                    }
+               }
+            }
         }
     }
 }
