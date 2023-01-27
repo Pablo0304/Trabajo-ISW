4c8468536677172f774cb91ac573ea5d4e9f7659
Pablo <gonca.pablo@gmail.com>
2023-01-28 00:08:10 +0100

    Ventana principal

--- Mereketengue/MereketengueInterfaz/ventanaprincipalNueva.cs
+++ Mereketengue/MereketengueInterfaz/ventanaprincipalNueva.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Magazine.Entities;
+using Magazine.Services;
+using MereketengueInterfaz.Properties;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +15,39 @@ namespace MereketengueInterfaz
 {
     public partial class ventanaprincipalNueva : Form
     {
-        public ventanaprincipalNueva()
+        private IMagazineISWService service;
+        private User userControl;
+        private Magazine.Entities.Magazine magazine;
+
+        public ventanaprincipalNueva(IMagazineISWService service)
         {
             InitializeComponent();
+            this.service = service;
+            userControl = service.getLoggedUser();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Login ventana = new Login(service);
+            this.Hide();
+            ventana.ShowDialog();
+            this.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SignUp ventana = new SignUp(service);
+            this.Hide();
+            ventana.ShowDialog();
+            this.Close();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            ConsultarIssueNOReg ventana = new ConsultarIssueNOReg(service);
+            this.Hide();
+            ventana.ShowDialog();
+            this.Close();
         }
     }
 }
